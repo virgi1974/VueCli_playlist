14585c7e4821f4837313493ba51a9a6bda20019e
virgilio1974 <vitrgilio1974@gmail.com>
2017-08-30 23:09:31 +0200

    component with list of heroes added to root element

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div> <!-- Component template should contain exactly one root element - so we wrap all components inside a div-->
     <app-header></app-header>
+    <app-heroes></app-heroes>
     <app-footer></app-footer>
   </div>
 </template>
@@ -9,12 +10,14 @@
 
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import Heroes from './Heroes.vue'
 
 export default {
   components: {
     // header or footer can not be used as a component name cause is a reserved word
     'app-header': Header,
     'app-footer': Footer,
+    'app-heroes': Heroes
   },
   data () {
     return {
--- src/Heroes.vue
+++ src/Heroes.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-    <h1>from heroes.vue component</h1>
+  <div id="heroes">
     <ul>
-      <li v-for="heroe in heroes">
-        {{heroe}}
+      <li v-for="heroe in heroes" v-on:click="heroe.show = !heroe.show">
+        <h2>
+          {{heroe.name}}
+        </h2>
+        <h3 v-if="heroe.show">{{heroe.power}}</h3>
       </li>
     </ul>
   </div>
@@ -13,14 +15,49 @@
 export default {
   data () {
     return {
-      heroes: ['Superman','Robin','CatWoman']
+      heroes: [
+        {name: 'Superman', power: 'strength', show: false},
+        {name: 'Batman', power: 'swift', show: false},
+        {name: 'Spiderman', power: 'sense of humor', show: false},
+        {name: 'Elektra', power: 'martial arts', show: false},
+        {name: 'Daredevil', power: 'sixth sense', show: false},
+        {name: 'Thor', power: 'hummer', show: false}
+      ]
     }
   }
 }
 </script>
 
 <style scoped>
-  h1 {
-    color:green;
+
+  #heroes {
+    width: 100%;
+    max-width: 1200px;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
   }
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    margin: 10px;
+    flex-grow: 1;
+    flex-basis: 300px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid #222;
+    border-radius: 25px;
+  }
+
+  h3 {
+    color: lightblue;
+  }
+
 </style>
