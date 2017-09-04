2f329bd7828ed10aa2cd3994b6c6743985061c8f
virgilio1974 <vitrgilio1974@gmail.com>
2017-09-04 06:49:12 +0200

    vue-resource dependency added with npm

--- src/main.js
+++ src/main.js
@@ -1,17 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// ----- import and setting a global component
-// import Heroes from './Heroes.vue'
+import VueResource from 'vue-resource'
+Vue.use(VueResource);
 
-// this way we register the component globaly
-// we register under the name "heroes"
-// Vue.component('heroes', Heroes);
-// --------------------------------------------
-
-//  Event Bus (to comunicate among child components)
-// to be used in the components we need to export it
-export const bus = new Vue();
 new Vue({
   el: '#app',
   render: h => h(App)
