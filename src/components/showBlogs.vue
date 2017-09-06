7d69e1d94a030b3380814244da9746ab395dccb4
virgilio1974 <vitrgilio1974@gmail.com>
2017-09-06 19:53:36 +0200

    adding filters

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,9 +1,9 @@
 <template>
-  <div v-theme:column="'wide'" id="show-blogs">
+  <div id="show-blogs">
     <h1>All Blog Articles</h1>
     <div v-for="blog in blogs" class="single-blog">
-      <h2 v-rainbow>{{ blog.title }}</h2>
-      <article>{{ blog.body}}</article>
+      <h2>{{ blog.title | to-uppercase}}</h2>
+      <article>{{ blog.body | short-version}}</article>
     </div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,26 +4,12 @@ import App from './App.vue'
 import VueResource from 'vue-resource'
 Vue.use(VueResource);
 
-// custom directive declared globally
-Vue.directive('rainbow',{
-  bind(el,binding,vnode){
-    el.style.color = "#" + Math.random().toString().slice(2,8);
-  }
+Vue.filter('to-uppercase', function(value){
+  return value.toUpperCase();
 })
 
-// custom directive to use different theme, with differnent values or arguments passed
-Vue.directive('theme',{
-  bind(el,binding,vnode){
-    if (binding.value == 'wide') {
-      el.style.maxWidth = "1200px";
-    }else if(binding.value == 'narrow'){
-      el.style.maxWidth = "600px";
-    };
-
-    if (binding.arg == 'column') {
-      el.style.background = 'floralwhite';
-    };
-  }
+Vue.filter('short-version', function(value){
+  return value.slice(0,100) + '....';
 })
 
 new Vue({
